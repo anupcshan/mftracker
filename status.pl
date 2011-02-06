2830514db29ab512649af3713a3ac08807280138
Anup C Shan <anupcshan@gmail.com>
2011-02-06 21:29:58 +0530

    Added support for displaying current status.

---
+++ status.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+
+use DBI;
+
+$dbh = DBI->connect("dbi:SQLite:dbname=/home/anup/.mftracker/mftracker.db", "", "");
+
+sub getstatusformf() {
+	my ($mfid, $mfname) = @_;
+	my $buyquery = "SELECT SUM(quantity), SUM(quantity * buyprice) FROM portfolio WHERE mfid = '$mfid'";
+	my $qresult = $dbh->selectall_arrayref($buyquery);
+	my ($quantity, $total) = @{@$qresult[0]};
+	my $avgbuyprice = $total / $quantity;
+	$avgbuyprice = (int(($avgbuyprice * 1000) + 0.5)) / 1000;
+	$total = (int(($total * 1000) + 0.5)) / 1000;
+
+	my $currentnavquery = "SELECT nav FROM navhistory WHERE mfid = '$mfid' AND date = (SELECT MAX(date) FROM navhistory WHERE mfid = '$mfid')";
+	$qresult = $dbh->selectall_arrayref($currentnavquery);
+	my ($currentprice) = @{@$qresult[0]};
+	my $currentvalue = $currentprice * $quantity;
+	printf ("%40s %8.3f %10.3f %8.3f %8.3f %10.3f\n", $mfname, $quantity, $total, $avgbuyprice, $currentprice, $currentvalue);
+	return ($total, $currentvalue);
+}
+
+sub getstatusbymf() {
+	print "=" x 89, "\n";
+	printf ("%40s %8s %10s %8s %8s %10s\n", 'Name', 'Units', 'Total', 'Avg Cost', 'Cur Cost', 'Cur Value');
+	print "-" x 89, "\n";
+	my $listportfoliosquery = "SELECT mfid, mfname FROM mfinfo WHERE mfid IN (SELECT DISTINCT mfid FROM portfolio)";
+	my $qresult = $dbh->selectall_arrayref($listportfoliosquery);
+	my $totalbuyvalue = 0, $totalcurrentvalue = 0;
+	for my $mfrow (@$qresult) {
+		my ($mfid, $mfname) = @$mfrow;
+		my ($buyvalue, $currentvalue) = &getstatusformf($mfid, $mfname);
+		$totalbuyvalue += $buyvalue;
+		$totalcurrentvalue += $currentvalue;
+	}
+	print "-" x 89, "\n";
+	printf ("%40s %19.3f %28.3f\n", 'Total', $totalbuyvalue, $totalcurrentvalue);
+	print "=" x 89, "\n";
+}
+
+&getstatusbymf();
