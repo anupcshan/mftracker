d2dc781a6dc84d924d628b1624e6049f88c7a834
Anup C Shan <anupcshan@gmail.com>
2011-06-15 16:25:31 +0530

    Added graph series to show difference between Sensex and MF values.

--- generate_graph.sh
+++ generate_graph.sh
@@ -14,9 +14,12 @@ cp $SENSEXCSV $SENSEXDAT
 sed -i 1d $SENSEXDAT
 for line in `cat $SENSEXDAT`
 do
+    date=`echo $line | grep -o "^[0-9-]*" | sed "s#-#/#g"`
+    profit=`grep "$date" $MFSTATUSFILE | grep -o "[-0-9.]*$"`
     today=`echo $line | grep -o "[0-9.]*$"`
     relative=`echo "scale=4; ($today-20301.10)/203.0110" | bc`
-    sed -i "s/$line/$line,$relative/" $SENSEXDAT
+    change=`echo "$profit - $relative" | bc`
+    sed -i "s/$line/$line,$relative,$change/" $SENSEXDAT
 done
 sed -i "s/,/ /g" $SENSEXDAT
 sed -i "s/\([0-9]\)\-/\1\//g" $SENSEXDAT
@@ -26,6 +29,7 @@ set xdata time
 set timefmt "%Y/%m/%d"
 plot "$MFSTATUSFILE" using 1:5 with lines title 'Profit'
 replot "$SENSEXDAT" using 1:8 with lines title 'Sensex'
+replot "$SENSEXDAT" using 1:9 with lines title 'Change'
 set terminal png size 1440,900
 set output "$DIR/graph.png"
 replot
