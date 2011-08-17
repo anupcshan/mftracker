7bb1c2c8fd9acd2c089ec24f0e9d529032f24ab2
Anup C Shan <anupcshan@gmail.com>
2011-08-17 20:07:31 +0530

    Cleaned up data file generation a bit.

--- generate_graph.sh
+++ generate_graph.sh
@@ -13,15 +13,16 @@ sed -i "s/,/ /g" $MFSTATUSFILE
 wget -c -q "http://ichart.finance.yahoo.com/table.csv?s=%5EBSESN&a=00&b=5&c=2011&g=d&ignore=.csv" -O $SENSEXCSV
 cp $SENSEXCSV $DATAFILE
 sed -i 1d $DATAFILE
-startsensex=`tail -n1 $DATAFILE | grep -o "[0-9.]*$"`
+firstdate=`cut -d ',' -f 1 $DATAFILE | sort | head -n1`
+startsensex=`grep "$firstdate" $DATAFILE | cut -d ',' -f 7`
 for line in `cat $DATAFILE`
 do
     date=`echo $line | grep -o "^[0-9-]*" | sed "s#-#/#g"`
-    profit=`grep "$date" $MFSTATUSFILE | grep -o "[-0-9.]*$"`
-    today=`echo $line | grep -o "[0-9.]*$"`
+    profit=`grep "$date" $MFSTATUSFILE | cut -d ' ' -f 5`
+    today=`echo $line | cut -d ',' -f 7`
     relative=`echo "scale=4; 100*($today-$startsensex)/$startsensex" | bc`
     change=`echo "$profit - $relative" | bc`
-    sed -i "s/$line/$line,$relative,$change/" $DATAFILE
+    sed -i "s#$line#$date,$today,$relative,$change#" $DATAFILE
 done
 sed -i "s/,/ /g" $DATAFILE
 sed -i "s/\([0-9]\)\-/\1\//g" $DATAFILE
@@ -30,8 +31,8 @@ cat << EOF > $DIR/gnuplot.scr
 set xdata time
 set timefmt "%Y/%m/%d"
 plot "$MFSTATUSFILE" using 1:5 with lines title 'Profit'
-replot "$DATAFILE" using 1:8 with lines title 'Sensex'
-replot "$DATAFILE" using 1:9 with lines title 'Change'
+replot "$DATAFILE" using 1:3 with lines title 'Sensex'
+replot "$DATAFILE" using 1:4 with lines title 'Change'
 replot 0 with lines title 'Zero'
 set terminal png size 1440,900
 set output "$DIR/graph.png"
